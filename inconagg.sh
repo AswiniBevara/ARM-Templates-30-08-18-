74796450af6eb50cdad98fc5d3bbeac2b4d4768a
AswiniBevara <35129047+AswiniBevara@users.noreply.github.com>
2018-09-12 14:48:01 +0530

    Update inconagg.sh

--- inconagg.sh
+++ inconagg.sh
@@ -6,10 +6,10 @@ sudo chmod +x install.sh
 ./install.sh
 cd ~/Aggregator
 sudo sed -i -e "s/<UpdateLocation>/East Us2/g" /home/adminuser/Aggregator/config.js
-sudo sed -i -e "s/<IOTHubConnectionString>/HostName=priyaiothubpukpu.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=59bt5Z8xI8d9sxonjmsIvWlYEnCznyisDbQEVGak4fI=/g" /home/adminuser/Aggregator/config.js
-sudo sed -i -e "s/<storageAccountName>/priyastrgaccntpukpu/g" /home/adminuser/Aggregator/config.js
-sudo sed -i -e "s/<storageAccountAccessKey>/2vwY6VMH4YSvAtr8RIdFbQwTJ+pcvFBYFf9kb0Jg1HQ9HsCMri2GsAfo\/N8yr+fSrVlGtdX8wq7Db4PGm8h6jA==/g" /home/adminuser/Aggregator/config.js
-sudo sed -i -e "s/<backendUrl>/https:\/\/priyarestapppukpu.azurewebsites.net/g" /home/adminuser/Aggregator/config.js
+sudo sed -i -e "s/<IOTHubConnectionString>/HostName=basiciothubruhqs.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=S6AX2uB3S6\/I9jmYGqG679qXMkGUDZzHUqjm6VzepzQ=/g" /home/adminuser/Aggregator/config.js
+sudo sed -i -e "s/<storageAccountName>/basicstrgaccntruhqs/g" /home/adminuser/Aggregator/config.js
+sudo sed -i -e "s/<storageAccountAccessKey>/\/xc+9mwnwys6dJz\/xV47W2KrO0YhjYEB\/\/c6wkdpLF4RFwF\/m2XspY1MuOYfzm0UGlg+BwK9hDPl3BPGlMocJA==/g" /home/adminuser/Aggregator/config.js
+sudo sed -i -e "s/<backendUrl>/https:\/\/basicrestappruhqs.azurewebsites.net/g" /home/adminuser/Aggregator/config.js
 sudo sed -i -e "s/<videoIndexingSubscriptionKey>/115b4fa4a2d0476988a1c0e419760554/g" /home/adminuser/Aggregator/config.js
 forever start aggregatorServer.js
 node aggregatorServer.js
